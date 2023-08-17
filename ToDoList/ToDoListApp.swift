c2b250d802c15a5e09550f9ff38246265d06da7d
M Montserrat Gomez <montserrat.ga95@gmail.com>
2023-08-17 14:52:58 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Montserrat Gomez on 2023-08-17.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
